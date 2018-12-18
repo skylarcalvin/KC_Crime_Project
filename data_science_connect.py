5613275861bfa40690d7c190c7a91f0faac78f2a
rducky01 <rducky01@gmail.com>
2018-12-18 05:25:35 -0600

    Added Jupyter Notebook for documentation.

--- data_science_connect.py
+++ data_science_connect.py
@@ -1,3 +1,9 @@
+#
+# Written by Skylar Calvin
+# year 2018
+# Connectes to postgres database on my AWS instance.
+#
+
 import getpass
 import psycopg2
 
@@ -5,7 +11,7 @@ mypasswd = getpass.getpass()
 
 connection = psycopg2.connect(database = 'data_science',
                               user = 'rducky',
-                              host = 'srv-skyhelm-01',
+                              host = 'datascience-test.ce7gijyt5i37.us-east-1.rds.amazonaws.com',
                               password = mypasswd)
 
 del mypasswd
