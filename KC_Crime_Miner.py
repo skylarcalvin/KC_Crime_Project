710f8776f480f919a16aef49a6ff4e68eb16836c
rducky01 <rducky01@gmail.com>
2019-04-14 11:58:06 -0500

    Starting Over

--- KC_Crime_Miner.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from sodapy import Socrata
-
-client = Socrata('data.kcmo.org', '2BlqNquer3Zje80NhkrNCNJyU')
-
-results = client.get('nyg5-tzkz')
-df = pd.DataFrame.from_dict(results)
-
-print(len(df))
--- data_science_connect.py
+++ data_science_connect.py
@@ -11,7 +11,7 @@ mypasswd = getpass.getpass()
 
 connection = psycopg2.connect(database = 'data_science',
                               user = 'rducky',
-                              host = 'datascience-test.ce7gijyt5i37.us-east-1.rds.amazonaws.com',
+                              host = 'calvin-tech.net',
                               password = mypasswd)
 
 del mypasswd
